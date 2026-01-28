356413f514e0ea38775117247608e79f30813233
Yimi Vasquez <yimi.vasquez@siemens-energy.com>
2026-01-28 19:40:02 +0000

    aula4

--- Aula 4 Modulos em Python/meu_modulo.py
+++ Aula 4 Modulos em Python/meu_modulo.py
@@ -5,3 +5,9 @@ def somar(x, y):
 def multiplicar(x, y):
     """Recebe dois números x e y e retorna sua multiplicação"""
     return x * y
+
+
+#1.  Adicionar a lista:
+minha_lista=list(range(1,50,3))
+#Adicionar a seguinte Varivel
+PT=10000000 
