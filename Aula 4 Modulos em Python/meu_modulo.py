3fab5dbcd41fb3a4aaad9ff2b01a67fc391d9598
Yimi Vasquez <yimi.vasquez@siemens-energy.com>
2026-01-28 19:29:11 +0000

    aula4

---
+++ Aula 4 Modulos em Python/meu_modulo.py
@@ -0,0 +1,7 @@
+def somar(x, y):
+    """Recebe dois números x e y e retorna sua soma"""
+    return x + y
+
+def multiplicar(x, y):
+    """Recebe dois números x e y e retorna sua multiplicação"""
+    return x * y
